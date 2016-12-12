3d0f1cb59f7abab2904bde881189c27ea3cf1519
Sonakshi Mehra <sonakshimehra.sona@gmail.com>
2016-12-13 02:23:30 +0530

    Adding code to serialize a binary tree into a 1D string

---
+++ Serialize-Inorder.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// Encodes a tree to a single string.
+string serialize(TreeNode* root) {
+  string s = "";
+  queue<TreeNode*> q;
+  if(!root) return "#";
+  s += to_string(root->val) + ",";
+  q.push(root);
+  while(!q.empty()) {
+    TreeNode *front = q.front();
+    q.pop();
+    if(!front->left) {
+      s += "#,";
+    }
+    else {
+      s += to_string(front->left->val) + ",";
+      q.push(front->left);
+    }
+    if(!front->right) {
+      s += "#,";
+    }
+    else {
+      s += to_string(front->right->val) + ",";
+      q.push(front->right);
+    }
+  }
+  s.pop_back();
+  return s;
+}
+
+int main() {
+  TreeNode *A = new TreeNode(5);
+	A->left = new TreeNode(4);
+	A->left->right = new TreeNode(6);
+	A->left->right->left = new TreeNode(8);
+	A->right = new TreeNode(9);
+	A->right->left = new TreeNode(10);
+  cout << serialize(A);
+}
